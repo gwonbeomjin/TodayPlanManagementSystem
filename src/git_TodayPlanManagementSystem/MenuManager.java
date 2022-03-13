6218508f60d872dc2072c4f6970e35f60210c0ee
gwonbeomjin <gb8783@naver.com>
2022-03-14 01:28:46 +0900

    added MenuManager

---
+++ src/git_TodayPlanManagementSystem/MenuManager.java
@@ -0,0 +1,15 @@
+package git_TodayPlanManagementSystem;
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("Today Plan Manager");
+		System.out.println("1. Add Plan");
+		System.out.println("2. Delete Plan");
+		System.out.println("3. Edit Plan");
+		System.out.println("4. View Plan");
+		System.out.println("5. Exit");
+
+	}
+
+}
